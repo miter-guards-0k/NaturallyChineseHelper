9e836ab6c1f590472b3547af7f24b55d9e2d8827
Michael Vu <email@michael.vu>
2023-03-04 22:48:49 +0000

    switch between dev mode and live for gallary

--- js/dev-mode.js
+++ js/dev-mode.js
@@ -1,3 +1,10 @@
 jQuery('document').ready(() => {
-    if ( !location.href.match(/mode=dev/) ) jQuery('.new-gallery').hide();
+    if ( location.href.match(/mode=dev/) ) {
+        jQuery('.old-gallery').hide();
+        jQuery('.new-gallery').show();
+    }
+    else {
+        jQuery('.old-gallery').show();
+        jQuery('.new-gallery').hide();
+    }
 });
