2d837e143f4d5f868e8e954943928932ecae0255
Michael Vu <email@michael.vu>
2023-03-05 09:37:15 +0000

    update

--- js/dev-mode.js
+++ js/dev-mode.js
@@ -1,10 +1,12 @@
 jQuery('document').ready(() => {
     if ( location.href.match(/mode=dev/) ) {
         jQuery('.old-gallery').hide();
-        jQuery('.new-gallery-*').show();
+        jQuery('.new-gallery-slider').show();
+        jQuery('.new-gallery-pictures').show();
     }
     else {
         jQuery('.old-gallery').show();
-        jQuery('.new-gallery-*').hide();
+        jQuery('.new-gallery-slider').hide();
+        jQuery('.new-gallery-pictures').hide();
     }
 });
